ffba6bfb38dc3a084fd74ffd50df5b8b152b9219
Gagan-Crownstack <gagan@crownstack.com>
2024-09-24 23:36:53 +0530

    final-commit

--- constant.js
+++ constant.js
@@ -1,27 +1,25 @@
-export const data1=[
-    {
-        name: "#1 DATA",
-        des1:`Using our data, insights and operations, we help the world’s  smallest vendors see outstanding growth in just a few months of activity – and shed light on what has been an operational “black hole” to the world’s largest consumer goods companies.`,
-        des2:`Utilizing a holistic technology led platform, with a strong eﬃcient network of shared local warehouses, we align supply and demand across the ecosystem.`
-    },
-    {
-        name:"#2 SALES",
-        des1:"We utilize our data so that small vendors enjoy unprecedented sales growth based on our insights. We offer vendors direct promotions from our manufacturers which is tailored to their needs based on our data."
+const https = require('https');
 
-    },
-    {
-        name:"#3 PARTNERS",
-        des1:`Partnerships with local wholesalers enable us to create an efficient network of regional warehouses with no capex.`,
-        des2:`Partnerships with global manufacturers enable us to significantly grow their sales and market share while they provide promotions for the vendors.`
-        
-    },
-    {
-        name:"#4 CREDITS",
-        des1:"Based on our network data and the unique needs of our value chain we have developed tailored AI-based credit scoring",
-        des2:`And marketing algorithms enabling the micro, small and medium businesses in our value chain to enjoy responsible and relevant credit products from our financial partners such as micro-retailer consignment or Wholesaler credit.`
-    },
-    {
-        name:"#5 TRADE FINANCE",
-        des1:`The &frnds technology enables us to bundle the entire value chain into a portfolio lending opportunity for financial institutions. In addition, we can provide financing to each of the small stores. The process is powered by our unique AI algorithms, which combines risk score (credit score) and propensity score. This service is already impacting tens of thousands of our users, creating efficiency and much better product prices to consumers.​`
-    }
-]
+const apiUrl= "https://pokeapi.co/api/v2/pokemon/ditto"
+
+https.get(apiUrl,(res)=>{
+    
+    let rawData = '';
+
+        res.on('data', (chunk) => {
+          rawData += chunk;
+        });
+
+
+        res.on('end', () => {
+            try {
+              const data = JSON.parse(rawData);
+              console.log(data);
+            } catch (e) {
+              reject(e.message);
+            }
+        })
+
+
+            
+})
--- script.js
+++ script.js
@@ -1,84 +1,201 @@
 // import {data1, data2} from './constant';
 
 //Data 
-const data=[
+
+const data = [
   {
-      name: "#1 DATA",
-      des1:`Using our data, insights and operations, we help the world’s  smallest vendors see outstanding growth in just a few months of activity – and shed light on what has been an operational “black hole” to the world’s largest consumer goods companies.`,
-      des2:`Utilizing a holistic technology led platform, with a strong eﬃcient network of shared local warehouses, we align supply and demand across the ecosystem.`
+    name: "#1 DATA",
+    des1: `Using our data, insights and operations, we help the world’s  smallest vendors see outstanding growth in just a few months of activity – and shed light on what has been an operational “black hole” to the world’s largest consumer goods companies.`,
+    des2: `Utilizing a holistic technology led platform, with a strong eﬃcient network of shared local warehouses, we align supply and demand across the ecosystem.`
   },
   {
-      name:"#2 SALES",
-      des1:"We utilize our data so that small vendors enjoy unprecedented sales growth based on our insights. We offer vendors direct promotions from our manufacturers which is tailored to their needs based on our data."
+    name: "#2 SALES",
+    des1: "We utilize our data so that small vendors enjoy unprecedented sales growth based on our insights. We offer vendors direct promotions from our manufacturers which is tailored to their needs based on our data."
 
   },
   {
-      name:"#3 PARTNERS",
-      des1:`Partnerships with local wholesalers enable us to create an efficient network of regional warehouses with no capex.`,
-      des2:`Partnerships with global manufacturers enable us to significantly grow their sales and market share while they provide promotions for the vendors.`
-      
+    name: "#3 PARTNERS",
+    des1: `Partnerships with local wholesalers enable us to create an efficient network of regional warehouses with no capex.`,
+    des2: `Partnerships with global manufacturers enable us to significantly grow their sales and market share while they provide promotions for the vendors.`
+
   },
   {
-      name:"#4 CREDITS",
-      des1:"Based on our network data and the unique needs of our value chain we have developed tailored AI-based credit scoring",
-      des2:`And marketing algorithms enabling the micro, small and medium businesses in our value chain to enjoy responsible and relevant credit products from our financial partners such as micro-retailer consignment or Wholesaler credit.`
+    name: "#4 CREDITS",
+    des1: "Based on our network data and the unique needs of our value chain we have developed tailored AI-based credit scoring",
+    des2: `And marketing algorithms enabling the micro, small and medium businesses in our value chain to enjoy responsible and relevant credit products from our financial partners such as micro-retailer consignment or Wholesaler credit.`
   },
   {
-      name:"#5 TRADE FINANCE",
-      des1:`The &frnds technology enables us to bundle the entire value chain into a portfolio lending opportunity for financial institutions. In addition, we can provide financing to each of the small stores. The process is powered by our unique AI algorithms, which combines risk score (credit score) and propensity score. This service is already impacting tens of thousands of our users, creating efficiency and much better product prices to consumers.​`
+    name: "#5 TRADE FINANCE",
+    des1: `The &frnds technology enables us to bundle the entire value chain into a portfolio lending opportunity for financial institutions. In addition, we can provide financing to each of the small stores. The process is powered by our unique AI algorithms, which combines risk score (credit score) and propensity score. This service is already impacting tens of thousands of our users, creating efficiency and much better product prices to consumers.​`
   }
 ]
 
 
-const data2=[
+const data2 = [
   {
-      id:1,
-      heading:"FMCG & Financial Companies",
-      subHeading:" Consortium partners include leading consumer brands and financial institutions",
-      src:"./Public/hdimg1.svg",
-      src2:""
+    id: 1,
+    heading: "FMCG & Financial Companies",
+    subHeading: " Consortium partners include leading consumer brands and financial institutions",
+    src: "./Public/hdimg1.svg",
+    src2: ""
   },
   {
-    id:2,
-    heading:"Distributors",
-    subHeading:"Supply FMCG Products to wholesalers​ ",
-    src:"./Public/hdimg2.svg",
-},
-{
-  id:3,
-  heading:` frnd& Local Wholesalers`,
-  subHeading:`The &frnds platform powers the network of local wholesalers i.e. shared warehouse infrastructure model`,
-  src:"./Public/hdimg3.svg",
-},
-{
-  id:4,
-  heading:"` frnd& Commerce Manager",
-  subHeading:`&frnds commerce managers power the on-ground commerce network`,
-  src:"./Public/hdimg4.svg",
+    id: 2,
+    heading: "Distributors",
+    subHeading: "Supply FMCG Products to wholesalers​ ",
+    src: "./Public/hdimg2.svg",
+  },
+  {
+    id: 3,
+    heading: ` frnd& Local Wholesalers`,
+    subHeading: `The &frnds platform powers the network of local wholesalers i.e. shared warehouse infrastructure model`,
+    src: "./Public/hdimg3.svg",
+  },
+  {
+    id: 4,
+    heading: "` frnd& Commerce Manager",
+    subHeading: `&frnds commerce managers power the on-ground commerce network`,
+    src: "./Public/hdimg4.svg",
 
-},
-{
-  id:5,
-  heading:"FMCG & Financial Companies",
-  subHeading:"Network of millions of mom-and-pop shops (e.g. warungs, sari saris) ",
-  src:"./Public/hdimg5.svg",
-},
-  
+  },
+  {
+    id: 5,
+    heading: "FMCG & Financial Companies",
+    subHeading: "Network of millions of mom-and-pop shops (e.g. warungs, sari saris) ",
+    src: "./Public/hdimg5.svg",
+  },
+
+]
+
+const data3 = [
+  {
+    id: 1,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem IASDASDpsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 2,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 3,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 4,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 5,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 6,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 8,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 9,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 10,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 11,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 12,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 13,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 14,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 15,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+  {
+    id: 16,
+    src: "./Public/perfetti-300.png",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  },
+]
+
+const data4 = [
+  {
+    id: 1,
+    des: `CGAP Pilots &frnds Innovative Digital Credit Model for MSEs`,
+    link_title: `READ ON CGAP `
+  },
+  {
+    id: 2,
+    des: `Microsoft and &frnds digitally enable and transform small and medium businesses in Indonesia: `,
+    link_title: `WATCH 2 MINUTE VIDEO`
+  },
+  {
+    id: 3,
+    des: `IFC, Mastercard, Coca-Cola Europacific Partners Indonesia and &frnds Team Up to Provide Working Capital Support to Indonesian Micro-Retailers`,
+    link_title: `READ ON IFC `
+  },
+  {
+    id: 4,
+    des: `&frnds CEO Dorron Mottes gives his insights on how digital tools & innovations help manage relationships with small merchants at AXA’s Live Innovation Talk`,
+    link_title: `WATCH ON YOUTUBE`
+  }
 ]
 
 
 
-
+//Navbar responsive
 const hamButton = document.getElementById("hamBurger");
 
 hamButton.addEventListener("click", () => {
-//   nav.classList.remove("");
-    let navbar= document.getElementById("navbar");
-    let list=document.getElementById("navlist");
-    let close= document.getElementById("close");
+  //   nav.classList.remove("");
+  let navbar = document.getElementById("navbar");
+  let list = document.getElementById("navlist");
+  let close = document.getElementById("close");
   navbar.classList.add("bg-sky-500")
   navbar.classList.add("absolute")
-//   navbar.classList.add("top-0")
+  //   navbar.classList.add("top-0")
   navbar.classList.add("h-screen")
   navbar.classList.add("w-full")
   navbar.classList.add("flex-col")
@@ -88,25 +205,26 @@ hamButton.addEventListener("click", () => {
 
 });
 
+//solutionDiv div
+document.getElementById('solutionDiv').innerHTML = data.map((point) => {
+  return (
 
-document.getElementById('solutionDiv').innerHTML=data.map((point)=>
-{return(
-  
- ` 
+    ` 
             <button class="accordion">${point?.name}</button>
             <div class="panel">
                <p>${point?.des1}</p>
-               <p>${point?.des2?point.des2:""}</p>
+               <p>${point?.des2 ? point.des2 : ""}</p>
             </div>
   `)
 }
 ).join('')
 
+//Accordian 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -121,11 +239,11 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-// console.log(data1,data2);
 
-document.getElementById('steps').innerHTML = data2.map((step)=>{
-  return(`
-    <div class="step-div scroll-pt=${step.id*10} cursor-pointer hover:bg-sky-700">
+// steps div elemnts
+document.getElementById('steps').innerHTML = data2.map((step) => {
+  return (`
+    <div class="step-div scroll-pt=${step.id * 10} cursor-pointer hover:bg-sky-700">
             
                <div class="flex justify-center"> <img src=${step?.src} alt="hdimg1"></div>
             
@@ -136,31 +254,89 @@ document.getElementById('steps').innerHTML = data2.map((step)=>{
     `)
 }).join('')
 
+//truncatetxt 
+const truncatetxt = (str) => {
+  if (str.length > 20) {
+    return (str.slice(0, 140) + "...");
+  } else {
+    return str;
+  }
 
-let slider= tns({
-  container: ".slider-partner",
-  "slideby": 1,
-    "nav": false,
+}
 
-    responsive:{
-      1600:{
-        items:4,
-        gutter:20,
 
-      },
-      1024:{
-        items:3,
-        gutter:20
-      },
-      768:{
-        items:2,
-        gutter:20
-      },
-      480:{
-        items:1,
-        gutter:20
-      }
+document.getElementById('slider').innerHTML = data3.map((slide) => {
+  return (`
+        <div class="slide flex min-w-80 h-full flex-col bg-white text-l p-4 gap-7">
+        <div class='flex flex-col items-center'>
+        <div class=" w-1/2 flex contain mb-5"><img src=${slide.src} alt=""></div>
+        <div class=" mx-auto text-l text-center">${truncatetxt(slide.des)}</div>
+        </div>
+        </div>        
+    `)
+}).join('');
+
+document.getElementById('press').innerHTML = data4.map((point) => {
+  return (
+    `
+    <div class="flex flex-col justify-between gap-16 items-center border-b-2 border-yellow-300 px-2 py-4 border-dotted w-full lg:[w-1/4 ] lg:border-y-0 lg:border-x-2 lg:py-0 lg:border-solid lg:gap-6 ">
+            <div class="w-5/6">${point.des}</div>
+            <div class="w-5/6 flex justify-between text-l cursor-pointer hover:opacity-80 text-cyan-500">
+            <div>${point.link_title}</div>
+            <div class="w-6"><img src="./Public/link.svg" alt="link"></div>
+            </div>
+        </div>
+    `
+  )
+}).join('')
+
+
+
+
+
+let currentIndex = 0;
+const slider = document.getElementById('slider');
+const slides = document.querySelectorAll('.slide');
+let autoInterval;
+
+
+
+function updateSlider() {
+  const slideWidth = slides[0].clientWidth + 50;
+  slider.scrollLeft = currentIndex * slideWidth;
+  console.log(slideWidth)
+}
+
+
+document.querySelector('.prevpg').addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSlider();
+  }
+});
+
+document.querySelector('.nxtpg').addEventListener('click', () => {
+  if (currentIndex < slides.length - 1) {
+    currentIndex++;
+    updateSlider();
+  }
+});
+
+function autoslide() {
+  startAutoSlide = setInterval(() => {
+
+    if (currentIndex < slides.length - 1) {
+      currentIndex++;
     }
+    else {
+      currentIndex = 0;
+    }
+    updateSlider();
+
+  }, 3000)
+}
 
 
-})
+
+updateSlider();
+autoslide();
